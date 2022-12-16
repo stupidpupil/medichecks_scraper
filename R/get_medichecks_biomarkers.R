551d072ab9d2f138f523645c406d22f21717597b
Adam Watkins <adam@stupidpupil.co.uk>
2022-12-16 12:09:43 +0000

    Tweak info included in biomarkers

--- R/get_medichecks_biomarkers.R
+++ R/get_medichecks_biomarkers.R
@@ -40,7 +40,7 @@ get_medichecks_biomarkers <- function(){
 		new_rows <- category_info$children |> 
 			tibble::tibble() |>  
 			tidyr::unnest_wider(1) |> 
-			dplyr::select(shopify_page_handle, name, description) |> 
+			dplyr::select(shopify_page_handle, name, description, short_description) |> 
 			dplyr::filter(!is.na(shopify_page_handle)) |>
 			dplyr::rename(biomarker_handle = shopify_page_handle) |>
 			dplyr::mutate(category = category_info$category$name)
@@ -50,8 +50,26 @@ get_medichecks_biomarkers <- function(){
 	}	
 
 
+	try_to_get_first_paragraph_of_description <- function(desc){
+		ret <- purrr::possibly(
+			function(desc){desc |> 
+				xml2::read_html() |> 
+				rvest::html_node("p") |> 
+				rvest::html_text2()},
+			NA_character_)(desc)
+
+		return(ret)
+	}
+
 	biomarkers <- biomarkers |> 
-		dplyr::mutate(biomarker_handle = biomarker_handle |> stringr::str_remove("^biomarkers\\-"))
+		dplyr::mutate(
+			biomarker_handle = biomarker_handle |> stringr::str_remove("^biomarkers\\-"),
+			short_description = dplyr::if_else(
+				is.na(short_description) | stringr::str_length(short_description) == 0, 
+				description |> try_to_get_first_paragraph_of_description(), 
+				short_description)
+		) |>
+		dplyr::select(-description) 
 
 	return(biomarkers)
 }
