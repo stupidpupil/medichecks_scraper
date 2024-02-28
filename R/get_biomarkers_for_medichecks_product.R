7753cce55afc5feb503a01a0e9325a8440878c55
Adam Watkins <adam@stupidpupil.co.uk>
2024-02-28 12:58:59 +0000

    Fix biomarkers query and slow down scraping

--- R/get_biomarkers_for_medichecks_product.R
+++ R/get_biomarkers_for_medichecks_product.R
@@ -1,9 +1,8 @@
 get_biomarkers_for_medichecks_product <- function(product_handle){
 	base_url <-  "https://www.medichecks.com/products/"
 
-	rvest::read_html(paste0(base_url, product_handle)) |> 
-		rvest::html_nodes(".c-product__details-biomarkers") |> 
-		rvest::html_nodes(".c-product__details-accordion-menu__accordion--summary-title") |> 
+	rvest::read_html(paste0(base_url, product_handle)) |> 	
+		rvest::html_nodes("#detailsBiomarkers p.text-lg") |>
 		rvest::html_text2() |> 
 		stringr::str_to_lower() |> 
 		stringr::str_replace_all("[^a-z0-9]+","-") |> 
--- R/get_medichecks_products.R
+++ R/get_medichecks_products.R
@@ -1,11 +1,11 @@
-get_medichecks_products <- function(){
+get_medichecks_products <- function(page_idx_range = 1L:4L){
 
   base_url <- "https://medichecks.com/collections/all/products.json?fields=id,handle,body_html,tags,variants&limit=250"
 
 
   products <- list()
 
-  for (page_idx in 1L:4L) {
+  for (page_idx in page_idx_range) {
     url_with_page <- paste0(base_url, "&page=", page_idx) # At some point this will probably stop working as they catch-up with Shopify's API
 
     new_products <- jsonlite::read_json(url_with_page)$products
@@ -14,7 +14,7 @@ get_medichecks_products <- function(){
       purrr::discard(function(p){p$product_type %in% c("Gift Cards", "Collection Method", "Clinic", "")})
 
     new_products <- new_products |>
-      purrr::map(function(p){list_from_medichecks_product_json(p)})
+      purrr::map(function(p){Sys.sleep(0.1); list_from_medichecks_product_json(p)})
 
     products <- c(products, new_products)
   }
