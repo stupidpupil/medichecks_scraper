1db33808eb6dae1b6c6ae5cdbe997e3a5385253b
Adam Watkins <adam@stupidpupil.co.uk>
2024-12-31 14:30:10 +0000

    Fix finding biomarkers

--- R/get_biomarkers_for_medichecks_product.R
+++ R/get_biomarkers_for_medichecks_product.R
@@ -2,10 +2,11 @@ get_biomarkers_for_medichecks_product <- function(product_handle){
 	base_url <-  "https://www.medichecks.com/products/"
 
 	rvest::read_html(paste0(base_url, product_handle)) |> 	
-		rvest::html_nodes("#detailsBiomarkers p.text-lg") |>
+		rvest::html_nodes("#block-bio_marker_WM3bDH p.mb-0") |>
 		rvest::html_text2() |> 
 		stringr::str_to_lower() |> 
 		stringr::str_replace_all("[^a-z0-9]+","-") |> 
-		stringr::str_replace_all("(^\\-|\\-$)","")
+		stringr::str_replace_all("(^\\-|\\-$)","") |>
+		purrr::discard(\(x) x == "learn-more")
 }
 
